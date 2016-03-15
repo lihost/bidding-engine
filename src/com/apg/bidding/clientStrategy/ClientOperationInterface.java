4b343500ee87c07bccf88f0e42ed16a7bdc1a05c
tsohil <tsohil@gmail.com>
2016-03-16 01:33:27 +0530

    strategy interface added

---
+++ src/com/apg/bidding/clientStrategy/ClientOperationInterface.java
@@ -0,0 +1,10 @@
+package com.apg.bidding.clientStrategy;
+
+import java.util.Scanner;
+
+import com.apg.bidding.User;
+
+public interface ClientOperationInterface {
+	
+	public void execute(Scanner sc, User testUser);
+}
