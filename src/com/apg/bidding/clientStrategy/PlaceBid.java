63f7b4a113b471d38d02d951081f7aa079bc905b
tsohil <tsohil@gmail.com>
2016-03-21 01:07:15 +0530

    adding code for Placing bid

---
+++ src/com/apg/bidding/clientStrategy/PlaceBid.java
@@ -0,0 +1,36 @@
+package com.apg.bidding.clientStrategy;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import com.apg.bidding.Bid;
+import com.apg.bidding.BiddingHandler;
+import com.apg.bidding.User;
+
+public class PlaceBid implements ClientOperationInterface {
+
+	@Override
+	public void execute(Scanner sc, User testUser) {
+		// TODO Auto-generated method stub
+		
+		try {
+			BiddingHandler bh = BiddingHandlerSingleton.getInstance();
+			
+			System.out.print("Insert auction id for which you want to place bid\n >>>: ");
+			long auctionId = sc.nextLong();
+			
+			System.out.print("Insert your bid amount\n >>>: ");
+			double bidPrice = sc.nextDouble();
+			
+			// creating bid
+			Bid bid = new Bid(testUser, auctionId, bidPrice);
+			
+			//placing bid
+			bh.placeBid(testUser, auctionId, bid);
+		} catch (InputMismatchException ime) {
+			System.err.println("Err - Wrong Input!");
+		}
+		
+	}
+
+}
