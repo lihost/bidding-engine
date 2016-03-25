d56b42159bf8ecde2f26d966aae448143480c57e
tsohil <tsohil@gmail.com>
2016-03-26 00:19:02 +0530

    comments added

--- src/com/apg/bidding/Util.java
+++ src/com/apg/bidding/Util.java
@@ -2,6 +2,12 @@ package com.apg.bidding;
 
 import java.text.SimpleDateFormat;
 
+/**
+ * Util class for handling date formats
+ * 
+ * @author sohil
+ *
+ */
 public class Util {
 
 	public static final String DATE_FORMAT = "dd-MM-yyyy'T'HH:mm";
