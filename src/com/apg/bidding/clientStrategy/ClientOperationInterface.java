a0f39f8c3a04e145ced8770072f65bb6ee0f0565
Sohil <tsohil@gmail.com>
2016-03-25 23:58:52 +0530

    Update ClientOperationInterface.java

--- src/com/apg/bidding/clientStrategy/ClientOperationInterface.java
+++ src/com/apg/bidding/clientStrategy/ClientOperationInterface.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 
 import com.apg.bidding.User;
 
+/**
+ * Strategy interface to facilitate various client operation
+ * @author sohil
+ *
+ */
 public interface ClientOperationInterface {
 	
 	public void execute(Scanner sc, User testUser);
